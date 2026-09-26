b611f2f294780a13c9b5e064972f46095660db5d
NITHISH CHELIAN <nithishchelian@gmail.com>
2026-09-26 18:38:48 +0530

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+public class Calculator {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int ans = 0;
+		while (true) {
+			System.out.println("enter a symbol for operation or press x for exit");
+			char ch = in.next().trim().charAt(0);
+			if (ch == '+' || ch == '-' || ch == '*' || ch == '/' || ch == '%') {
+				System.out.println("enter two number");
+				int a = in.nextInt();
+				int b = in.nextInt();
+				if (ch == '+') {
+					ans = a + b;
+				}
+				if (ch == '-') {
+					ans = a - b;
+				}
+				if (ch == '*') {
+					ans = a * b;
+				}
+				if (ch == '/') {
+					if (b == 0) {
+						System.out.print("zero is invalid in denominator");
+
+
+					} else {
+						ans = a / b;
+
+					}
+				}
+				if (ch == '%') {
+					ans = a % b;
+				}
+
+				System.out.println(ans);
+
+
+			}  else if (ch == 'x' || ch == 'X') {
+				break;
+
+
+			}
+
+
+
+			else {
+				System.out.println("invalid operator");
+			}
+
+
+		}
+
+
+	}
+}
